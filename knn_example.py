bb657772f5c522c5f68900cecd19d481bb590dfc
bob81135 <bob81135@gmail.com>
2020-03-24 17:13:46 +0800

    knn example base dataset digits

---
+++ knn_example.py
@@ -0,0 +1,17 @@
+from sklearn import datasets, svm, metrics
+from sklearn.neighbors import KNeighborsClassifier
+
+digits = datasets.load_digits()
+
+n_samples = len(digits.images)
+
+# 資料攤平:1797 x 8 x 8 -> 1797 x 64
+# 這裏的-1代表自動計算，相當於 (n_samples, 64)
+data = digits.images.reshape((n_samples, -1))
+
+knn = KNeighborsClassifier()
+
+knn.fit(data[:n_samples-100], digits.target[:n_samples-100])
+
+print("Confusion matrix:\n%s"
+    % metrics.confusion_matrix(knn.predict(data[n_samples-100:]), digits.target[n_samples-100:]))
